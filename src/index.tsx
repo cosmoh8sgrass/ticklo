82e681dd0a4659f2209c5768a2c62ec36a98c8c5
Kar6677thik <karthik.nttp@gmail.com>
2025-10-12 16:25:07 +0530

    v2

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from './ThemeContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "./ThemeContext";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
